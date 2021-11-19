0617716d6c58340e5f6fbf8292ec29e3f1859cb2
Alexander <40055399+alex-borisi@users.noreply.github.com>
2021-11-19 12:47:05 +0300

    Update update-309_to_310.php

--- sys/upgrade/update/update-309_to_310.php
+++ sys/upgrade/update/update-309_to_310.php
@@ -1,7 +1,131 @@
 <?php 
 
+/**
+ * Удаление устаревших таблиц базы данных
+ * */
+
 db::query("DROP TABLE `user_group`"); 
-db::query("DROP TABLE `all_accesses`"); 
+db::query("DROP TABLE `all_accesses`");
+
+/**
+ * Обновляем новые группы доступа
+ * */
+
+db::query("TRUNCATE `user_group_access`"); 
+
+$user_group_access = array(
+	array('id_group' => '9','id_access' => 'adm_accesses'),
+	array('id_group' => '15','id_access' => 'adm_accesses'),
+	array('id_group' => '3','id_access' => 'adm_banlist'),
+	array('id_group' => '11','id_access' => 'adm_banlist'),
+	array('id_group' => '12','id_access' => 'adm_banlist'),
+	array('id_group' => '6','id_access' => 'adm_banlist'),
+	array('id_group' => '12','id_access' => 'adm_info'),
+	array('id_group' => '4','id_access' => 'adm_info'),
+	array('id_group' => '6','id_access' => 'adm_info'),
+	array('id_group' => '11','id_access' => 'adm_info'),
+	array('id_group' => '9','id_access' => 'adm_info'),
+	array('id_group' => '8','id_access' => 'adm_info'),
+	array('id_group' => '15','id_access' => 'adm_info'),
+	array('id_group' => '5','id_access' => 'adm_info'),
+	array('id_group' => '2','id_access' => 'adm_info'),
+	array('id_group' => '3','id_access' => 'adm_panel_show'),
+	array('id_group' => '6','id_access' => 'adm_panel_show'),
+	array('id_group' => '11','id_access' => 'adm_panel_show'),
+	array('id_group' => '4','id_access' => 'adm_panel_show'),
+	array('id_group' => '9','id_access' => 'adm_panel_show'),
+	array('id_group' => '8','id_access' => 'adm_panel_show'),
+	array('id_group' => '15','id_access' => 'adm_panel_show'),
+	array('id_group' => '12','id_access' => 'adm_panel_show'),
+	array('id_group' => '7','id_access' => 'adm_panel_show'),
+	array('id_group' => '2','id_access' => 'adm_panel_show'),
+	array('id_group' => '5','id_access' => 'adm_panel_show'),
+	array('id_group' => '2','id_access' => 'adm_ref'),
+	array('id_group' => '5','id_access' => 'adm_ref'),
+	array('id_group' => '8','id_access' => 'adm_set_sys'),
+	array('id_group' => '15','id_access' => 'adm_set_sys'),
+	array('id_group' => '9','id_access' => 'adm_set_sys'),
+	array('id_group' => '2','id_access' => 'adm_show_adm'),
+	array('id_group' => '12','id_access' => 'adm_show_adm'),
+	array('id_group' => '4','id_access' => 'adm_show_adm'),
+	array('id_group' => '11','id_access' => 'adm_show_adm'),
+	array('id_group' => '6','id_access' => 'adm_show_adm'),
+	array('id_group' => '3','id_access' => 'adm_show_adm'),
+	array('id_group' => '3','id_access' => 'adm_statistic'),
+	array('id_group' => '12','id_access' => 'adm_statistic'),
+	array('id_group' => '2','id_access' => 'adm_statistic'),
+	array('id_group' => '6','id_access' => 'adm_statistic'),
+	array('id_group' => '4','id_access' => 'adm_statistic'),
+	array('id_group' => '5','id_access' => 'adm_statistic'),
+	array('id_group' => '11','id_access' => 'adm_statistic'),
+	array('id_group' => '9','id_access' => 'adm_themes'),
+	array('id_group' => '15','id_access' => 'adm_themes'),
+	array('id_group' => '9','id_access' => 'adm_users_list'),
+	array('id_group' => '7','id_access' => 'adm_users_list'),
+	array('id_group' => '8','id_access' => 'adm_users_list'),
+	array('id_group' => '15','id_access' => 'adm_users_list'),
+	array('id_group' => '15','id_access' => 'forum_comment_delete'),
+	array('id_group' => '15','id_access' => 'forum_comment_edit'),
+	array('id_group' => '15','id_access' => 'forum_delete'),
+	array('id_group' => '15','id_access' => 'forum_edit'),
+	array('id_group' => '15','id_access' => 'forum_topic_delete'),
+	array('id_group' => '15','id_access' => 'forum_topic_edit'),
+	array('id_group' => '9','id_access' => 'plugins'),
+	array('id_group' => '15','id_access' => 'plugins'),
+	array('id_group' => '9','id_access' => 'update_core'),
+	array('id_group' => '15','id_access' => 'update_core'),
+	array('id_group' => '11','id_access' => 'user_ban_set'),
+	array('id_group' => '9','id_access' => 'user_ban_set'),
+	array('id_group' => '12','id_access' => 'user_ban_set'),
+	array('id_group' => '7','id_access' => 'user_ban_set'),
+	array('id_group' => '2','id_access' => 'user_ban_set'),
+	array('id_group' => '8','id_access' => 'user_ban_set'),
+	array('id_group' => '15','id_access' => 'user_ban_set'),
+	array('id_group' => '5','id_access' => 'user_ban_set_h'),
+	array('id_group' => '2','id_access' => 'user_ban_set_h'),
+	array('id_group' => '11','id_access' => 'user_ban_set_h'),
+	array('id_group' => '3','id_access' => 'user_ban_set_h'),
+	array('id_group' => '12','id_access' => 'user_ban_set_h'),
+	array('id_group' => '6','id_access' => 'user_ban_set_h'),
+	array('id_group' => '8','id_access' => 'user_ban_unset'),
+	array('id_group' => '15','id_access' => 'user_ban_unset'),
+	array('id_group' => '7','id_access' => 'user_ban_unset'),
+	array('id_group' => '2','id_access' => 'user_ban_unset'),
+	array('id_group' => '9','id_access' => 'user_ban_unset'),
+	array('id_group' => '8','id_access' => 'user_delete'),
+	array('id_group' => '15','id_access' => 'user_delete'),
+	array('id_group' => '9','id_access' => 'user_delete'),
+	array('id_group' => '9','id_access' => 'user_edit'),
+	array('id_group' => '8','id_access' => 'user_edit'),
+	array('id_group' => '15','id_access' => 'user_edit'),
+	array('id_group' => '9','id_access' => 'user_files_delete'),
+	array('id_group' => '15','id_access' => 'user_files_delete'),
+	array('id_group' => '8','id_access' => 'user_files_delete'),
+	array('id_group' => '7','id_access' => 'user_files_delete'),
+	array('id_group' => '7','id_access' => 'user_files_edit'),
+	array('id_group' => '9','id_access' => 'user_files_edit'),
+	array('id_group' => '15','id_access' => 'user_files_edit'),
+	array('id_group' => '8','id_access' => 'user_files_edit'),
+	array('id_group' => '9','id_access' => 'user_group'),
+	array('id_group' => '15','id_access' => 'user_group'),
+	array('id_group' => '2','id_access' => 'user_prof_edit'),
+	array('id_group' => '3','id_access' => 'user_prof_edit'),
+	array('id_group' => '5','id_access' => 'user_prof_edit'),
+	array('id_group' => '3','id_access' => 'user_show_add_info'),
+	array('id_group' => '2','id_access' => 'user_show_add_info'),
+	array('id_group' => '3','id_access' => 'user_show_ip'),
+	array('id_group' => '3','id_access' => 'user_show_ua'),
+	array('id_group' => '2','id_access' => 'user_show_ua'),
+	array('id_group' => '6','id_access' => 'user_show_ua')
+);
+
+foreach($user_group_access AS $data) {
+	db::insert('user_group_access', $data); 
+}
+
+/**
+ * Удаление устаревших файлов
+ * */
 
 $delete_files = array(
 	'adm_panel/adm_log.php', 
@@ -41,7 +165,6 @@ $delete_files = array(
 	'sys/upgrade/db_install/all_accesses.sql', 
 	'sys/upgrade/db_install/user_group.sql', 
 ); 
-
 
 foreach($delete_files AS $file) {
 	if (is_file(ROOTPATH . '/' . $file)) {
